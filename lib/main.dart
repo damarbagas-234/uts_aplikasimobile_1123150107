a6540d6f78794de43781d02b9ac9566ad0a85301
damarbagas-234 <muhammaddamar107@gmail.com>
2025-11-07 19:51:43 +0700

    nambahin nim sama nama lengkap

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "NIM : 1123150107, Muhammad Damar Bagas",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
