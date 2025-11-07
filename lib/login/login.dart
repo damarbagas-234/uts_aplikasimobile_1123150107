9e945f5eed1009a67cb5932f3ac6d010b85580ec
damarbagas-234 <muhammaddamar107@gmail.com>
2025-11-07 20:32:53 +0700

    nambahin button login

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -105,6 +105,26 @@ class Login extends StatelessWidget {
                         ),
                       ),
                     ),
+                    SizedBox(height: 10,),
+                    Container(
+                      child: SizedBox(
+                        height: 40,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: (){
+                              
+                          }, 
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                          ),
+                          child: Text('Login',
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
