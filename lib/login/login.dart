78a999b1798d480a419d38408bdb55a2c319c8a2
damarbagas-234 <muhammaddamar107@gmail.com>
2025-11-07 20:32:01 +0700

    nambahin text kamu lupa pass?

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -93,6 +93,18 @@ class Login extends StatelessWidget {
                         ),
                       ),
                     ),
+                    SizedBox(height: 10,),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {}, 
+                          child: Text('Kamu Lupa Password ?', 
+                          style: TextStyle(
+                            color: Colors.deepPurple
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
